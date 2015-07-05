8462ec45eae217399a1775280b88e08d941fc9d3
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2015-07-05 12:33:15 +0300

    Fix setup.py and requirements.pip for the first release

--- automate_rpc/__init__.py
+++ automate_rpc/__init__.py
@@ -16,6 +16,15 @@
 # You should have received a copy of the GNU General Public License
 # along with automate_arduino.  If not, see <http://www.gnu.org/licenses/>.
 
+__author__ = "Tuomas Airaksinen"
+__copyright__ = "Copyright 2015, Tuomas Airaksinen"
+__credits__ = []
+__license__ = "GPL"
+__version__ = '0.9'
+__maintainer__ = "Tuomas Airaksinen"
+__email__ = "tuomas.airaksinen@gmail.com"
+__status__ = "Beta"
+
 from .rpc import RpcService
 
 extension_classes = [RpcService]
--- setup.py
+++ setup.py
@@ -4,21 +4,25 @@ from setuptools import setup, find_packages
 from pip.req import parse_requirements
 install_reqs = parse_requirements('requirements.pip')
 
+def get_version(filename):
+    import re
+    with open(filename) as fh:
+        metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", fh.read()))
+        return metadata['version']
+
 setupopts = dict(
     name="automate_rpc",
-    version="0.1",
+    version=get_version('automate_rpc/__init__.py'),
     packages=find_packages(),
 
     install_requires=[str(ir.req) for ir in install_reqs],
 
-
-    # metadata for upload to PyPI
     author="Tuomas Airaksinen",
     author_email="tuomas.airaksinen@gmail.com",
-    description="Automate webui",
-    long_description="automate webui long description",
-    license="Free for non-commercial use",
-    keywords="automation, GPIO, Raspberry Pi, RPIO, enaml, traits",
+    description="Remote Procedure Call Support for Automate",
+    long_description="This Automate extension provides XmlRPC API for external application",
+    license="GPL",
+    keywords="automation, GPIO, Raspberry Pi, RPIO, traits",
     url="http://github.com/tuomas2/automate_webui",
     entry_points={'automate.extension': [
             'rpc = automate_rpc:extension_classes'
@@ -26,14 +30,11 @@ setupopts = dict(
 
     classifiers=["Development Status :: 4 - Beta",
                  "Environment :: Console",
-                 "Environment :: X11 Applications :: Qt",
-                 "Environment :: Win32 (MS Windows)",
                  "Environment :: Web Environment",
                  "Intended Audience :: Education",
-                 "Intended Audience :: End Users/Desktop",
                  "Intended Audience :: Developers",
                  "Intended Audience :: Information Technology",
-                 "License :: Free for non-commercial use",
+                 "License :: OSI Approved :: GNU General Public License (GPL)",
                  "Operating System :: Microsoft :: Windows",
                  "Operating System :: POSIX",
                  "Programming Language :: Python :: 2.7",
