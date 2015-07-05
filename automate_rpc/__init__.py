241191233ab26fdd6410f9f5dda8d9727dff6609
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2015-07-05 15:40:47 +0300

    Edit licence note in all files

--- automate_rpc/__init__.py
+++ automate_rpc/__init__.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 # (c) 2015 Tuomas Airaksinen
 #
-# This file is part of automate_arduino.
+# This file is part of automate-rpc.
 #
-# automate_arduino is free software: you can redistribute it and/or modify
+# automate-rpc is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# automate_arduino is distributed in the hope that it will be useful,
+# automate-rpc is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with automate_arduino.  If not, see <http://www.gnu.org/licenses/>.
+# along with automate-rpc.  If not, see <http://www.gnu.org/licenses/>.
 
 __author__ = "Tuomas Airaksinen"
 __copyright__ = "Copyright 2015, Tuomas Airaksinen"
